fd9e1bd1d141c6541dcca8254455f3e8cab7d280
unknown <rrmail@ukr.net>
2023-05-17 16:05:16 +0300

    CREATED: start structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from '/clicktweet.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={logo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+  )
+}
+
+export default App
