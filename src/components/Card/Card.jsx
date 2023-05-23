45ddfd06894c59da3a957e606cc30a82a4aef1e6
unknown <rrmail@ukr.net>
2023-05-23 09:59:18 +0300

    CHANGE: logic

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,8 +8,7 @@ import "./Card.css";
 const { VITE_AVATAR_URL } = import.meta.env;
 
 const Card = (tweet) => {
-  const { id, tweets, followers, avatar } = tweet;
-  let following = tweet.following;
+  const { id, tweets, followers, avatar, following } = tweet;
   const currentUser = useCurrentUser((state) => state.currentUser);
   const setCurrentUser = useCurrentUser((state) => state.setCurrentUser);
   const setFollowing = useUserList((state) => state.setFollowing);
@@ -17,19 +16,18 @@ const Card = (tweet) => {
   const [notActive, setNotActive] = useState(false);
 
   const handleFollowing = async () => {
-    following = !following;
-    let followList;
-    await setFollowing({ id, following });
-    following
-      ? (followList = [...currentUser.follow_list, id])
-      : (followList = currentUser.follow_list.filter((el) => el !== id));
+    const followList = following
+      ? currentUser.follow_list.filter((el) => el !== id)
+      : [...currentUser.follow_list, id];
+    const fls = following ? followers - 1 : followers + 1;
+    await setFollowing({ id, following: !following });
     await setCurrentUser({ ...currentUser, follow_list: followList });
     try {
       const { data: s1 } = await iUser.put(`/owners/${currentUser.id}`, {
         follow_list: followList,
       });
       const { data: s2 } = await iUser.put(`/users/${id}`, {
-        followers,
+        followers: fls,
       });
       !(!s2 || !s1) && setNotActive(false);
     } catch (e) {
@@ -41,7 +39,7 @@ const Card = (tweet) => {
   return (
     <div className="card">
       <img src={logo} className="card-logo" alt="card logo" width={76} />
-      <img src={decor} className="decor" alt="decor" width={308} />
+      <img src={decor} className="decor" width={308} />
       <div className="avatar-line">
         <img src={aurl} className="avatar" alt="avatar" width={64} />
       </div>
